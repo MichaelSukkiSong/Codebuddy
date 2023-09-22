d9c05a8b67e3f1aad6b40aaf82c7d376d4735c5b
송석기 <sa77irak@gmail.com>
2023-09-21 20:31:37 -0700

    made create category button and cleaned up a comment

---
+++ codebuddy_client/src/components/Modal.js
@@ -0,0 +1,10 @@
+const Modal = ({ onClose }) => {
+  return (
+    <div>
+      <div>Modal</div>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+};
+
+export default Modal;
--- codebuddy_client/src/components/Sidebar.js
+++ codebuddy_client/src/components/Sidebar.js
@@ -1,4 +1,6 @@
 import './Sidebar.scss';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
 import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { TbBulb } from 'react-icons/tb';
@@ -10,9 +12,12 @@ import { BsLightningCharge } from 'react-icons/bs';
 import { BsGlobe } from 'react-icons/bs';
 import { GrTest } from 'react-icons/gr';
 import { TbArrowsExchange } from 'react-icons/tb';
+import { AiOutlinePlus } from 'react-icons/ai';
 import { clearMessagesExceptOne } from '../store';
+import Modal from './Modal';
 
 const Sidebar = () => {
+  const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
 
   const handleSidebarClick = () => {
@@ -133,6 +138,19 @@ const Sidebar = () => {
           </NavLink>
         </li>
       </ul>
+      <div className="sidebar__btn">
+        <button onClick={() => setShowModal(true)}>
+          <div className="plus-icon">
+            <AiOutlinePlus />
+          </div>
+          <span>Create Category</span>
+        </button>
+        {showModal &&
+          createPortal(
+            <Modal onClose={() => setShowModal(false)} />,
+            document.body
+          )}
+      </div>
     </div>
   );
 };
