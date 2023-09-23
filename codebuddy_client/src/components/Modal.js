e96296f4e2cc784b076dc24a9ae00b70d21ad2d3
송석기 <sa77irak@gmail.com>
2023-09-22 21:13:48 -0700

    styled modal window

--- codebuddy_client/src/components/Modal.js
+++ codebuddy_client/src/components/Modal.js
@@ -4,9 +4,19 @@ const Modal = ({ onClose }) => {
   return (
     <div className="modal">
       <div className="modal__content">
-        <div className="modal__content--form">Some form</div>
+        <div className="modal__content--form">
+          <div className="modal__content--form-name">
+            <label>Name</label>
+            <input />
+          </div>
+          <div className="modal__content--form-prompt">
+            <label>Prompt</label>
+            <textarea />
+          </div>
+        </div>
         <div className="modal__content--btn">
-          <button onClick={onClose}>Close</button>
+          <button>Create</button>
+          <button onClick={onClose}>Cancel</button>
         </div>
       </div>
     </div>
