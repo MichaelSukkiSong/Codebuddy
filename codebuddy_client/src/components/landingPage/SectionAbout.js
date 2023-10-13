f53ed5490277193ed9c2e055346f830bb39ad3af
송석기 <sa77irak@gmail.com>
2023-10-13 16:41:53 -0700

    changed about section structure and added typewritter effect

--- codebuddy_client/src/components/landingPage/SectionAbout.js
+++ codebuddy_client/src/components/landingPage/SectionAbout.js
@@ -1,4 +1,5 @@
 import './SectionAbout.scss';
+import Typewriter from 'typewriter-effect';
 import aboutImage1 from '../../images/photo-1684369175833-4b445ad6bfb5.jpg';
 import aboutImage2 from '../../images/data-center-network-3wz76kas9x1nwy42.jpeg';
 
@@ -8,40 +9,66 @@ const SectionAbout = () => {
       <div className="section__about">
         <h1 className="section__about--header">What it does</h1>
         <div className="section__about--part">
-          <div className="section__about--part-text">
-            <div className="section__about--text">
-              <h2>An AI friend right by your side</h2>
-              Codebuddy helps you with your code by pre-configuring all the
-              boring, monotonous, repetitive text that you have to type before
-              you actually ask the real question to GPT
-            </div>
+          <div className="section__about--part-title">
+            <h2>An AI friend right by your side</h2>
           </div>
-          <div className="section__about--part-image">
-            <div className="section__about--image">
-              <img
-                className="section__about--image-1"
-                src={aboutImage1}
-                alt="about image1"
-              />
+          <div className="section__about--part-content">
+            <div className="section__about--part-text">
+              <div className="section__about--text">
+                <Typewriter
+                  options={{
+                    strings: [
+                      'Codebuddy helps you with your code by pre-configuring all the boring, monotonous, repetitive text that you have to type before you actually ask the real question to GPT',
+                    ],
+                    autoStart: true,
+                    loop: true,
+                    pauseFor: 5500,
+                    deleteSpeed: 1,
+                    delay: 10,
+                  }}
+                />
+              </div>
+            </div>
+            <div className="section__about--part-image">
+              <div className="section__about--image">
+                <img
+                  className="section__about--image-1"
+                  src={aboutImage1}
+                  alt="about image1"
+                />
+              </div>
             </div>
           </div>
         </div>
         <div className="section__about--part">
-          <div className="section__about--part-image">
-            <div className="section__about--image">
-              <img
-                src={aboutImage2}
-                alt="about image2"
-                className="section__about--image-1"
-              />
-            </div>
+          <div className="section__about--part-title">
+            <h2>Save your chat</h2>
           </div>
-          <div className="section__about--part-text">
-            <div className="section__about--text">
-              <h2>Save your chat</h2>
-              Codebuddy can also save your responses to a database for future
-              reference and can also organize, manage your knowledge base in a
-              user-friendly way.
+          <div className="section__about--part-content">
+            <div className="section__about--part-image">
+              <div className="section__about--image">
+                <img
+                  src={aboutImage2}
+                  alt="about image2"
+                  className="section__about--image-1"
+                />
+              </div>
+            </div>
+            <div className="section__about--part-text">
+              <div className="section__about--text">
+                <Typewriter
+                  options={{
+                    strings: [
+                      'Codebuddy can also save your responses to a database for future reference and can also organize, manage your knowledge base in a user-friendly way.',
+                    ],
+                    autoStart: true,
+                    loop: true,
+                    pauseFor: 10000,
+                    deleteSpeed: 1,
+                    delay: 10,
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
